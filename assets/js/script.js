ecf8dbc7869db6478b492bec64eae0716c8267bf
SteffB23 <sbrookens@hotmail.com>
2021-10-08 01:50:59 -0400

    Added styles to font in css; added form for employee ID# & Password along with a submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,9 +64,5 @@ var details = data.map(function(data) {
 
 info.innerHTML = details.join(' ');
 
-/*//Working on form Object Below lines 68 on:
-var moreInfo = document.querySelector('#more-info')
-var form = data.
-
-moreInfo.innerHTML = details.join(' ');*/
+//Work on event listeners here:
 
