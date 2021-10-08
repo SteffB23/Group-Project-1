8c09b0611d02a3ae032b6e6047334ca1ad8ef5b7
SteffB23 <sbrookens@hotmail.com>
2021-10-07 21:23:35 -0400

    added preliminary employee menu page with branding scheme

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+JS:
+
+//Array of objects:
+var form = "Enter Addition Information here"
+var data = [
+    {
+      name: 'Pizza',
+      cost: '$3.00'
+    },
+
+    {
+      name: 'Salad',
+      cost: '$7.50'
+    },
+ 
+    {
+      name: 'Pasta Bowl',
+      cost: '$8.99'
+    },
+
+    {
+      name: 'Rice and Beans',
+      cost: '$4.99'
+    },
+
+    {
+      name: '5 peice Chicken Wings',
+      cost: '$5.39'
+    },
+
+
+    {
+      name: 'Hot Soup',
+      cost: '$2.00'
+    },
+
+    {
+      name: 'Tacos',
+      cost: '$1.49 each'
+    },
+
+        {
+      name: 'Drink',
+      cost: '$1.99'
+    },
+
+    {
+      name: 'Bag of Chips',
+      cost: '$.99'
+    },
+    {
+      name: 'Surprise Me',
+      cost: '$5.00'
+    },
+
+]
+
+
+var info = document.querySelector('#info')
+
+var details = data.map(function(data) {
+    return (
+        '<div>' + data.name + ' ' + ' will cost ' + data.cost + ' Plus Tax' + '</div>'
+    )
+});
+
+info.innerHTML = details.join(' ');
+
+//Working on form Object Below lines 68 on:
+var moreInfo = document.querySelector('#more-info')
+var form = data.
+
+moreInfo.innerHTML = details.join(' ');
