aa460859365c968ad663526a15918a0edfab5f93
SteffB23 <sbrookens@hotmail.com>
2021-10-13 21:45:50 -0400

    added sample animations for team to choose from

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -1,15 +1,28 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter'],
+            },
+            colors: {
+               gray: colors.blueGray, 
+            },
+            animate: {
+    //4 values: 1-name of animation 2- length 3-Timing function 4-amount of cycles         
+                spin: "spin 5s linear infinite",
+                pulse: "pulse 1s linear infinite",
+                ping: "ping 1s linear infinite",
+                bounce: "bounce 1s linear infinite",
+                wiggle: "wiggle 1s ease-in-out infinite",
+
+
+            },
+            keyframes: {},
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: {},
 }
+
