88e6be94a74b6a275fa66866c866b887acb11d4f
Gus199 <augustine2903a@gmail.com>
2021-10-16 14:17:20 -0400

    Adding 3 pages to the original Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,1 @@
-var pizzaEl =document.querySelector('.max-h-12-1');
-
-pizzaEl.addEventListener('click', function select(e){
-    console.log("test");
-
-});
-function select(){
-    fetch('')
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-         
-    })
-}
 
