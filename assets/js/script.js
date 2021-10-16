11214e55521b15249f62f293f0c4736ca2eff99a
Gus199 <augustine2903a@gmail.com>
2021-10-16 10:22:04 -0400

    Adding the weather Api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,42 @@
 
 var pizzaEl =document.querySelector('.max-h-12-1');
 
+var weatherContainer = document.getElementById('weather');
+
+const format2 = "YYYY-MM-DD"
+var date2 = new Date();
+dateTime2 = moment(date2).format(format2);
+document.getElementById("demo2").innerHTML = "As of :"+"" +dateTime2;
+
+
+
+
+
+function getApiW(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=40&lon=-75&appid=6939a1ebe83748be1cc4bbe471aef1a9&units=imperial')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+console.log(data);
+var wthEl = document.createElement('h2');
+wthEl.setAttribute('style', ' display: block;font-size: 3.5rem;line-height: .96em;font-weight: 500;');
+
+
+
+wthEl.textContent =data.main.temp+"°";
+weatherContainer.append(wthEl);
+
+    })
+}
+getApiW();
+
+
+
+
+
+
+
 pizzaEl.addEventListener('click', function select(e){
     console.log("test");
 
