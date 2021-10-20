45c308b43992e07cc94b2902e454fa8207c1f401
Gus199 <augustine2903a@gmail.com>
2021-10-20 18:37:03 -0400

    Rename Pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,4 @@
 
-// // MainPageNew Start Here
-// // MainPageNew Start Here
-
-// var orderNowbtn =document.getElementById('myBtn-left');
-
-// orderNowbtn.addEventListener('click',mainMenu);
-
-// function mainMenu(){
-//     window.location = 'MenuPageOrigin.html'
-// }
-// var orderNowbtn =document.getElementById('myBtn-right');
-
-// orderNowbtn.addEventListener('click',orderNow);
-
-// function orderNow(){
-//     window.location = 'infoPage.html'
-// }
-// // MainPageNew ENDs Here
-// // MainPageNew ENDs Here
-
 
 
 var pizzaEl =document.querySelector('.max-h-12-1');
@@ -72,109 +52,6 @@ function select(){
          
     })
 }
-// Get User curent Location:
-
-var locationEl = document.getElementById('getLocation')
- var newDiv = document.getElementById('myLocation')
-
-
-
-
-
-
-
-function getApiGeo(){ 
-
-locationEl.addEventListener('click', ()=>{
-    navigator.geolocation.getCurrentPosition(data =>{
-        console.log(data);
-    },(error)=>console.log(error))
-    
-    
-});
-
-function success(data){
-
-    var api_key = '8cf6f7e105304f8c975adee408dc60d8';
-    var latitude = data.coords.latitude;
-    var longitude = data.coords.longitude;
-  
-    var api_url = 'https://api.opencagedata.com/geocode/v1/json'
-  
-    var request_url = api_url
-      + '?'
-      + 'key=' + api_key
-      + '&q=' + encodeURIComponent(latitude + ',' + longitude)
-      + '&pretty=1'
-      + '&no_annotations=1';
-  
-    // see full list of required and optional parameters:
-    // https://opencagedata.com/api#forward
-  
-    var request = new XMLHttpRequest();
-    request.open('GET', request_url, true);
-  
-    request.onload = function() {
-      // see full list of possible response codes:
-      // https://opencagedata.com/api#codes
-  
-      if (request.status === 200){ 
-        // Success!
-        var data = JSON.parse(request.responseText);
-        // replace alert by an Element tag
-
-        // var locationEl= document.getElementById('location');
-        // locationEl.textContent="My current Address :" + ""+data.results[0].formatted;
-
-        // lets store the address and use it for confirmation.
-        var address =data.results[0].formatted;
-        localStorage.setItem('Address',JSON.stringify(address));
-        // alert(data.results[0].formatted); // print the location
-  
-      } else if (request.status <= 500){ 
-        // We reached our target server, but it returned an error
-                             
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log('error msg: ' + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-  
-    request.onerror = function() {
-      // There was a connection error of some sort
-      console.log("unable to connect to server");        
-    };
-  
-    request.send();  // make the request
-}
-navigator.geolocation.getCurrentPosition(success, console.error);
-}
-getApiGeo();
-
-
-// get User location
-var enterLocation = document.getElementById('getLocation');
-enterLocation.addEventListener('click',getloc);
-
-function getloc(){
-
-
-   var texTag = document.createElement('h2');
-   texTag.textContent = "Is this your current Location :"+ ''+ JSON.parse(localStorage.getItem('Address'));
-   newDiv.append(texTag);
-    
-   var yesButton =document.createElement('button');
-var noButton =document.createElement('button');
-yesButton.textContent = 'Yes'
-noButton.textContent = 'No'
-newDiv.append(yesButton);
-newDiv.append(noButton);
-}
-
-
-
 
 // For main Menu Start From here
 
@@ -217,12 +94,28 @@ function chooseMenu3(){
     console.log('test')
     window.location= "Salad.html"
 }
+
+// sandwiches :
+var sandwichEl = document.querySelector('.max-h-12-4')
+console.log(sandwichEl)
+
+
+sandwichEl.addEventListener('click', chooseMenu4);
+  
+
+
+function chooseMenu4(){
+    console.log('test')
+    window.location= "sandwich.html"
+}
+
+
 // Main Menu End here:
 
 var homeButton = document.getElementById('homeBtn');
 homeButton.addEventListener('click', function(e){
   console.log("test")
-window.location = "MainPageNew.html";
+window.location = "index.html";
 
 
 });
